a7eaa4acec0f2defe3fa33d37b99d9f855311987
username <nomadicalli@mail.ru>
2022-01-17 16:46:24 +0300

    Создал объект man

---
+++ script.js
@@ -0,0 +1,12 @@
+let man = {
+    name: "John",
+    surname: "Wick",
+    age: 35,
+    height: 1.80,
+    massa: 80,
+    login: "glanas",
+    password: 232425,
+    checkMoney: 200000,
+
+   
+}
